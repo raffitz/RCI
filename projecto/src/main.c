90f431ac94b0aad03774ebb42f303865ea8a175a
raffitz <raf.a.m.c.gon@gmail.com>
2015-03-03 19:00:51 +0000

    Added a few documentation-related changes

--- projecto/src/main.c
+++ projecto/src/main.c
@@ -1,7 +1,11 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+/** Função principal do programa ddt. Esta função serve-se dos módulos criados
+ *  nos restantes ficheiros-fonte para implementar as funcionalidades
+ *  necessárias ao projecto, como especificadas no enunciado.
+ */
 int main(){
-	printf("Testing");
+	printf("Testing\n");
 	exit(0);
 }
